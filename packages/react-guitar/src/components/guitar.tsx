a4f7cd234dcb2c09932c49419f3ac99dd247bbaf
Carlos Lancha <carlos.lancha@liferay.com>
2020-07-22 02:36:48 +0200

    Add default chords and keyboard events to trigger them

--- packages/react-guitar/src/components/guitar.tsx
+++ packages/react-guitar/src/components/guitar.tsx
@@ -15,14 +15,136 @@ export default function Guitar(props: Props) {
     lefty = false,
     renderFinger
   } = props
+
+  const pressedKeys = useRef([Number])
+  const currentChord = useRef({
+    chord: Number,
+    fif: false,
+    sev: false,
+    nin: false
+  })
+
   const fretsNodeRef = useRef(null as HTMLOListElement | null)
   const fretNodesRef = useRef({} as { [K: number]: HTMLLIElement | null })
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event: any) => {
+      if (!pressedKeys.current.includes(event.keyCode)) {
+        pressedKeys.current.push(event.keyCode)
+      }
+
+      const currentChord = {
+        chord: Number,
+        fif: false,
+        sev: false,
+        nin: false
+      }
+
+      pressedKeys.current.forEach((key: any) => {
+        if (key >= 65 && key <= 71) {
+          currentChord.chord = key
+        } else if (key === 53) {
+          currentChord.fif = true
+        } else if (key === 55) {
+          currentChord.sev = true
+        } else if (key === 57) {
+          currentChord.nin = true
+        }
+      })
+
+      let strings
+
+      switch (currentChord.chord as any) {
+        case 67: // C
+          strings = currentChord.fif
+            ? [-1, -1, -1, 5, 3, -1]
+            : currentChord.sev
+            ? [0, 1, 3, 2, 3, 0]
+            : currentChord.nin
+            ? [0, 3, 3, 2, 3, -1]
+            : [0, 1, 0, 2, 3, 0]
+          break
+        case 68: // D
+          strings = currentChord.fif
+            ? [-1, -1, -1, 7, 5, -1]
+            : currentChord.sev
+            ? [2, 1, 2, 0, 0, 0]
+            : currentChord.nin
+            ? [0, 1, 2, 0, -1, -1]
+            : [2, 3, 2, 0, 0, 0]
+
+          props.onChange?.([2, 3, 2, 0, 0, 0])
+          break
+        case 69: // E
+          strings = currentChord.fif
+            ? [-1, -1, -1, 9, 7, -1]
+            : currentChord.sev
+            ? [0, 0, 1, 0, 2, 0]
+            : currentChord.nin
+            ? [2, 0, 1, 0, 2, 0]
+            : [0, 0, 1, 2, 2, 0]
+          break
+        case 70: // F
+          strings = currentChord.fif
+            ? [-1, -1, -1, -1, 3, 1]
+            : currentChord.sev
+            ? [1, 1, 2, 1, 3, 1]
+            : currentChord.nin
+            ? [3, 1, 2, 1, 3, 1]
+            : [1, 1, 2, 3, 3, 1]
+          break
+        case 71: // G
+          strings = currentChord.fif
+            ? [-1, -1, -1, -1, 5, 3]
+            : currentChord.sev
+            ? [1, 0, 0, 0, 2, 3]
+            : currentChord.nin
+            ? [1, 0, 2, 0, -1, -1]
+            : [3, 0, 0, 0, 2, 3]
+          break
+        case 65: // A
+          strings = currentChord.fif
+            ? [-1, -1, -1, -1, 7, 5]
+            : currentChord.sev
+            ? [0, 2, 0, 2, 0, 0]
+            : currentChord.nin
+            ? [3, 2, 4, 2, 0, -1]
+            : [0, 2, 2, 2, 0, 0]
+          break
+        case 66: // B
+          strings = currentChord.fif
+            ? [-1, -1, -1, -1, 9, 7]
+            : currentChord.sev
+            ? [2, 0, 2, 1, 2, 0]
+            : currentChord.nin
+            ? [2, 2, 2, 1, 2, -1]
+            : [2, 4, 4, 4, 2, 2]
+          break
+      }
+
+      if (strings) {
+        props.onChange?.(strings)
+      }
+    })
+
+    document.addEventListener('keyup', (event: any) => {
+      const keyPosition = pressedKeys.current.indexOf(event.keyCode)
+
+      if (keyPosition !== -1) {
+        pressedKeys.current.splice(keyPosition, 1)
+      }
+    })
+  }, [])
+
   useEffect(() => {
     const fretsNode = fretsNodeRef.current
+
     if (fretsNode) {
       const toFret =
         min(strings.map(fret => fret).filter(fret => fret > 0)) || 1
+
       const fretNode = fretNodesRef.current[toFret]
+
       if (fitFretting && fretNode) {
         if (lefty) {
           if (fretNode.offsetLeft > 0) {
@@ -39,6 +161,7 @@ export default function Guitar(props: Props) {
       }
     }
   }, [fretsNodeRef, fretNodesRef, strings])
+
   return (
     <div className={classNames('guitar', { lefty }, props.className)}>
       <ol className="frets" ref={fretsNodeRef}>
@@ -68,6 +191,7 @@ export default function Guitar(props: Props) {
               }
               onPlay={string => props.onPlay?.(string)}
             />
+
             {fret !== 0 && <span className="counter">{fret}</span>}
           </li>
         ))}
