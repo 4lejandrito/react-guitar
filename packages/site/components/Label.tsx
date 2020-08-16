c1c271efb581aea76f8518b87fcaac850906ab82
Alejandro Tardín <alejandro@tardin.com>
2020-08-16 10:18:33 +0200

    Adding support for case configuration

--- packages/site/components/Label.tsx
+++ packages/site/components/Label.tsx
@@ -3,12 +3,17 @@ import classnames from 'classnames'
 
 export default function Label(props: {
   className?: string
-  name: string
+  name: string | ReactNode
+  lowercase?: boolean
   children: ReactNode
 }) {
   return (
     <label className={classnames('p-2 flex flex-col', props.className)}>
-      <small className="text-center font-bold text-gray-600 uppercase mb-2">
+      <small
+        className={classnames('text-center font-bold text-gray-600 mb-2', {
+          uppercase: !props.lowercase
+        })}
+      >
         {props.name}
       </small>
       <div className="h-10 flex items-center justify-center">
