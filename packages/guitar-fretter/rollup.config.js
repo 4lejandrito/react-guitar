df05c4a0d0b04fe8d2908da4747fa3dc481e7981
Alejandro Tardín <alejandro@tardin.com>
2020-08-17 12:51:50 +0200

    Adding basic fretter package

---
+++ packages/guitar-fretter/rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from 'rollup-plugin-typescript2'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
+    }
+  ],
+  plugins: [typescript()],
+  external: [...Object.keys(pkg.dependencies)]
+}
