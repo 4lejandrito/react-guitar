2fb27606f9dc3a8f8cef7fdee94190e7aee403bb
Alejandro Tardín <alejandro@tardin.com>
2020-10-07 07:40:52 +0200

    Adding initial snapshot test

---
+++ packages/react-guitar/test/react-guitar.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { generateImage } from 'jsdom-screenshot'
+import { render } from '@testing-library/react'
+import Guitar from '../src/index'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
+
+expect.extend({ toMatchImageSnapshot })
+
+it('renders the right HTML', () => {
+  expect(
+    renderer.create(<Guitar strings={[0, 0, 1, 2, 2, 0]} />).toJSON()
+  ).toMatchSnapshot()
+})
+
+it('has no visual regressions', async () => {
+  render(<Guitar strings={[0, 0, 1, 2, 2, 0]} />)
+  expect(await generateImage()).toMatchImageSnapshot()
+})
