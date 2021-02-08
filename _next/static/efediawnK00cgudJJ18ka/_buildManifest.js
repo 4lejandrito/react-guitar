c6f35a38f190e7f974efd29988436f74d8fa8fdf
4lejandrito <alejandro@tardin.com>
2021-02-08 07:29:46 +0000

    Deploying to gh-pages from  @ 503d043eb91f2fcf3ba80912dedbc72e6dbde8bf 🚀

---
+++ _next/static/efediawnK00cgudJJ18ka/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8928ea983aa7c9f248cf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cf7ef89afc48767dc7d0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
